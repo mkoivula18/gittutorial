1caf119749cf9045a04b2f4c397c104c1490a824
Mikael <karvonenmiksu@gmail.com>
2018-11-09 14:08:29 +0200

    juu

--- mathtools.py
+++ mathtools.py
@@ -19,4 +19,12 @@ def factorial(n):
         return n * factorial(n-1)
 		
 		
-#asdasd
+
+def arithmetic(a, difference, n):
+    '''Calculates the sum of a arithmetic serie of n elements.
+       An arithmetic sequence is of the form: a, a+d, a+2d, a+3d,...
+       n is the number of elements in the sequence.'''
+    #Get the arithmetic sequence
+    sequence = [a+difference*x for x in range(n)]
+    #Calculates its sum
+    return sum(sequence)
