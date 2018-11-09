be6f40ffbaf71f6b12205b02efce1c8c9a54ed03
Mikael <karvonenmiksu@gmail.com>
2018-11-09 14:04:28 +0200

    yolo

--- mathtools.py
+++ mathtools.py
@@ -19,4 +19,12 @@ def factorial(n):
         return n * factorial(n-1)
 		
 		
-#asdasd
+
+def fib(n):
+    ''' Calculates the n value of the fibonacci sequence'''
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1)+fib(n-2)
